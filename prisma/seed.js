8d8c7133970a36fec68736f40c8c574a512b15f9
Your Name <you@example.com>
2023-06-26 13:27:39 -0300

    Modificações para a apresentação

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,26 +6,31 @@ const capituloList = [
   {
     nome: "CS",
     logo: '1687710807111_CS-IEEE UFRN Symbol.png',
-    ramoId: 1
+    path: "/SobreCS",
+	ramoId: 1
   },
   {
     nome: "PELS/IAS",
     logo: '1687710826569_Logo PELS_IAS UFRN.png',
-    ramoId: 1
+    path: "/SobrePelsias",
+	ramoId: 1
   },
   {
     nome: "WIE",
     logo: '1687710836600_LOGO WIE FUNDO TRANSPARENTE.png',
-    ramoId: 1
+    path: "/SobreWie",
+	ramoId: 1
   },
   {
     nome: "In Class",
     logo: '1687710847548_Logo_In_Class.png',
-    ramoId: 1
+    path: "/SobreInClass",
+	ramoId: 1
   },
   {
     nome: "RAS",
 	logo: "1687778560008_LOGO RAS.png",
+	path: "/SobreRAS",
 	ramoId: 1
   }
 ];
@@ -35,64 +40,55 @@ const postList = [
 		tipo: PostType.NOTICIA,
 		titulo: "Confraternização IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687780408222_img.png",
-		usuarioId: 1
+		imagem: "1687780408222_img.png"
 	},
 	{
 		tipo: PostType.EVENTO,
 		titulo: "Curso IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687781479353_img.jpg",
-		usuarioId: 1
+		imagem: "1687781479353_img.jpg"
 	},
 	{
 		tipo: PostType.PROJETO,
 		titulo: "IEEE Irmão",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782055912_IEEE IrmÃ£o.jpeg",
-		usuarioId: 1
+		imagem: "1687782055912_IEEE IrmÃ£o.jpeg"
 	},
 	{
 		tipo: PostType.NOTICIA,
 		titulo: "Confraternização IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782160439_img.png",
-		usuarioId: 1
+		imagem: "1687782160439_img.png"
 	},
 	{
 		tipo: PostType.NOTICIA,
 		titulo: "Confraternização IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782163286_img.png",
-		usuarioId: 1
+		imagem: "1687782163286_img.png"
 	},
 	{
 		tipo: PostType.EVENTO,
 		titulo: "Evento IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		imagem: "1687782177648_img.jpg",
-		usuarioId: 1
 	},
 	{
 		tipo: PostType.EVENTO,
 		titulo: "Evento IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782178255_img.jpg",
-		usuarioId: 1
+		imagem: "1687782178255_img.jpg"
 	},
 	{
 		tipo: PostType.PROJETO,
 		titulo: "Evento IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782203214_IEEE IrmÃ£o.jpeg",
-		usuarioId: 1
+		imagem: "1687782203214_IEEE IrmÃ£o.jpeg"
 	},
 	{
 		tipo: PostType.PROJETO,
 		titulo: "Evento IEEE",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		imagem: "1687782203886_IEEE IrmÃ£o.jpeg",
-		usuarioId: 1
+		imagem: "1687782203886_IEEE IrmÃ£o.jpeg"
 	}
 ];
 
@@ -121,8 +117,7 @@ async function main() {
         nome: "IEEE",
         logo: "1687710503812_logo-44.png"
     }
-  }).then(cadastrarCapitulo)
-  .then(cadastrarUsuario).then(cadastrarPost)
+  }).then(cadastrarCapitulo).then(cadastrarUsuario).then(cadastrarPost)
 }
 
 main()
